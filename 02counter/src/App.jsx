d792a9edbf1194592a703675b70b36b50cd032b6
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-08-19 12:24:56 +0600

    Complete: Why you need hooks and project.

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const [counter, setCounter] = useState(15)
+
+  // let counter = 15;
+
+  function addValue() {
+    console.log("clicked: ", counter);
+
+    if (counter >= 20) {
+      return;
+    }
+
+    // counter += 1;
+    setCounter(counter + 1);
+  }
+
+  function removeValue() {
+    console.log("clicked: ", counter);
+
+    if (counter <= 0) {
+      return;
+    }
+
+    // counter -= 1;
+    setCounter(counter - 1);
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+      <button
+        onClick={(addValue)}
+        >
+        Add value {counter}
+      </button>
+      <button 
+      onClick={removeValue}
+      >
+        Remove value {counter}
+      </button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
