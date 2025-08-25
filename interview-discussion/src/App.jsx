0b1711c5215a001bda9d5822a7e07d3474c95529
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-08-25 23:41:16 +0600

    Complete: Testing your datatype skills in react interviews

--- interview-discussion/src/App.jsx
+++ interview-discussion/src/App.jsx
@@ -4,26 +4,43 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [value, setValue] = useState(1);
-  // const [multipliedValue, setMultipliedValue] = useState(1);
-  let multipliedValue = value * 5;
+  console.log("App rendered", Math.random());
 
-  function multiplyByFive() {
-    // setMultipliedValue(value * 5);
-    setValue(value + 1);
-  }
+  const [value, setValue] = useState({
+    value: 0,
+  });
+  // const [multipliedValue, setMultipliedValue] = useState(1);
+  // let multipliedValue = value * 5;
+
+  // function multiplyByFive() {
+  //   // setMultipliedValue(value * 5);
+  //   setValue(value + 1);
+  // }
+
+  const clickMe = () => {
+    // console.log("multiplyByFive");
+    // setValue(value + 1);
+    // setValue(1);
+    // setValue(3);
+    setValue({
+      value: 0,
+    });
+  };
 
   // Over engineering
   // useEffect(() => {
   //   setMultipliedValue(value * 5);
   // }, [value]);
 
+  useEffect(() => {
+    console.log("App rendered", Math.random());
+  }, [value.value]);
   return (
     <>
-      <h1>Main value: {value} </h1>
-      <button onClick={multiplyByFive}>Click to multipliy by 5</button>
+      <h1>Main value: {value.value} </h1>
+      <button onClick={clickMe}>Click to multipliy by 5</button>
       {/* <h2>Multiplied value: {multipliedValue}</h2> */}
-      <h2>Multiplied value: {multipliedValue}</h2>
+      {/* <h2>Multiplied value: {multipliedValue}</h2> */}
     </>
   );
 }
